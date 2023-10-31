624dda57495272d3d8a5c14f47a0191a575e8fd2
Mayumi Harada <you@example.com>
2023-10-31 15:45:05 +0900

    1030 commit.

--- convert.py
+++ convert.py
@@ -11,7 +11,7 @@ def prd_generate_substrings(text):
     # tagger = MeCab.Tagger('-r /dev/null -d /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir')
     # tagger = MeCab.Tagger()
     # tagger = MeCab.Tagger('-r /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir -d /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir')
-    tagger = MeCab.Tagger('-r /C:/Users/Harada/myenv/Lib/site-packages/MeCab/etc -d /C:/Users/Harada/myenv/Lib/site-packages/MeCab/dic/ipadic')
+    tagger = MeCab.Tagger('-r C:/Users/Harada/myenv/Lib/site-packages/MeCab/etc -d C:/Users/Harada/myenv/Lib/site-packages/MeCab/dic/ipadic')
     # 品詞分解を行う
     result = tagger.parse(text)
 
