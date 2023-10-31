fabcec75772e03300dca47c53867cd1456e1e2e8
Mayumi Harada <you@example.com>
2023-10-31 15:02:31 +0900

    1030 commit.

--- convert.py
+++ convert.py
@@ -10,7 +10,8 @@ def prd_generate_substrings(text):
     # tagger = MeCab.Tagger("-r /usr/local/etc/mecabrc -d /usr/local/lib/mecab/dic/unidic/")
     # tagger = MeCab.Tagger('-r /dev/null -d /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir')
     # tagger = MeCab.Tagger()
-    tagger = MeCab.Tagger('-r /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir -d /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir')
+    # tagger = MeCab.Tagger('-r /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir -d /C:/Users/Harada/myenv/Lib/site-packages/unidic/dicdir')
+    tagger = MeCab.Tagger('-r /C:/Users/Harada/myenv/Lib/site-packages/MeCab/etc -d /C:/Users/Harada/myenv/Lib/site-packages/MeCab/dic/ipadic')
     # 品詞分解を行う
     result = tagger.parse(text)
 
